667489da0c2e94c3345da0b4749538ff63fd2572
Rafal <wwww@wp.pl>
2018-11-17 10:47:07 +0100

    zmiana nazwy w modelu property

--- HomeBroker/HomeBroker/Models/Property.cs
+++ HomeBroker/HomeBroker/Models/Property.cs
@@ -8,7 +8,7 @@ namespace HomeBroker.Models
     public class Property
     {
         public int Id { get; set; }
-        public PropertyTyp PropertyTyp { get; set; }
+        public PropertyTyp Typ { get; set; }
         public string Description { get; set; }
         public int Room { get; set; }
         public int Area { get; set; }
